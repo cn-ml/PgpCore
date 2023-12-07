2fc102b6ed1b7f17527e40589e32a70a83ca9143
Michael Chen <git@cnml.de>
2023-12-07 14:47:56 +0100

    Add detach signature frontend

--- PgpCore/PGP.SignSync.cs
+++ PgpCore/PGP.SignSync.cs
@@ -215,5 +215,90 @@ namespace PgpCore
         public string ClearSignArmoredString(string input, IDictionary<string, string> headers = null) => ClearSign(input, headers);
 
         #endregion ClearSign
+
+        #region DetachSign
+
+        /// <summary>
+        /// Detach sign the file pointed to by unencryptedFileInfo
+        /// </summary>
+        /// <param name="inputFile">Plain data file to be signed</param>
+        /// <param name="outputFile">Output PGP signature file</param>
+        /// <param name="headers">Optional headers to be added to the output</param>
+        public void DetachSign(
+            FileInfo inputFile,
+            FileInfo outputFile,
+            IDictionary<string, string> headers = null)
+        {
+            if (inputFile == null)
+                throw new ArgumentException("InputFile");
+            if (outputFile == null)
+                throw new ArgumentException("OutputFile");
+            if (EncryptionKeys == null)
+                throw new ArgumentException("EncryptionKeys");
+            if (headers == null)
+                headers = new Dictionary<string, string>();
+
+            if (!inputFile.Exists)
+                throw new FileNotFoundException($"Input file [{inputFile.Name}] does not exist.");
+
+            using (Stream outputStream = outputFile.OpenWrite())
+            {
+                OutputDetachSigned(inputFile, outputStream, headers);
+            }
+        }
+
+        /// <summary>
+        /// Detach sign the provided stream
+        /// </summary>
+        /// <param name="inputStream">Plain data stream to be signed</param>
+        /// <param name="outputStream">Output PGP signature stream</param>
+        /// <param name="headers">Optional headers to be added to the output</param>
+        public void DetachSign(
+            Stream inputStream,
+            Stream outputStream,
+            IDictionary<string, string> headers = null)
+        {
+            if (inputStream == null)
+                throw new ArgumentException("InputStream");
+            if (outputStream == null)
+                throw new ArgumentException("OutputStream");
+            if (EncryptionKeys == null)
+                throw new ArgumentException("EncryptionKeys");
+            if (inputStream.Position != 0)
+                throw new ArgumentException("inputStream should be at start of stream");
+            if (headers == null)
+                headers = new Dictionary<string, string>();
+
+            OutputDetachSigned(inputStream, outputStream, headers);
+        }
+
+        /// <summary>
+        /// Detach sign the provided string
+        /// </summary>
+        /// <param name="input">Plain string to be signed</param>
+        /// <param name="headers">Optional headers to be added to the output</param>
+        public string DetachSign(
+            string input,
+            IDictionary<string, string> headers = null)
+        {
+            if (headers == null)
+                headers = new Dictionary<string, string>();
+
+            using (Stream inputStream = input.GetStream())
+            using (Stream outputStream = new MemoryStream())
+            {
+                DetachSign(inputStream, outputStream, headers);
+                outputStream.Seek(0, SeekOrigin.Begin);
+                return outputStream.GetString();
+            }
+        }
+
+        public void DetachSignFile(FileInfo inputFile, FileInfo outputFile, IDictionary<string, string> headers = null) => DetachSign(inputFile, outputFile, headers);
+
+        public void DetachSignStream(Stream inputStream, Stream outputStream, IDictionary<string, string> headers = null) => DetachSign(inputStream, outputStream, headers);
+
+        public string DetachSignArmoredString(string input, IDictionary<string, string> headers = null) => DetachSign(input, headers);
+
+        #endregion DetachSign
     }
 }
